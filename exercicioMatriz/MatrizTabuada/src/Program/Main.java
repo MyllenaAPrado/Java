5fd981aef1a0b6455699bc6c4e8ca9819bcd8bf5
MyllenaAPrado <myllena.pradoa@gmail.com>
2020-02-21 14:02:11 -0300

    modify the print of multiplication table

--- exercicioMatriz/MatrizTabuada/src/Program/Main.java
+++ exercicioMatriz/MatrizTabuada/src/Program/Main.java
@@ -14,11 +14,25 @@ public class Main {
 		}
 		
 		//print matrix
-		System.out.println("  1   2   3   4   5   6    7     8     9  ");
+		System.out.println("     1   2   3    4   5   6    7     8     9     10");
 		for(int i=0; i < matriz.length; i++) {
 			System.out.print((i+1) + " ");
 			for(int j=0; j< matriz[i].length; j++) {
-				System.out.print(matriz[i][j] + "     ");
+				
+				if( i ==  j) {
+					System.out.print("  |" + matriz[i][j] + "|");
+				}
+				else if(i == matriz.length - 1 ){
+					
+					System.out.print(" " + matriz[i][j] + "  ");
+				}
+				else if(j >1){
+					System.out.print(" " + matriz[i][j] + "  ");
+				}
+				else{
+					System.out.print("  " + matriz[i][j] + "  ");
+				}
+				
 			}
 			System.out.println();
 		}
